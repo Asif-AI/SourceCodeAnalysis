66844b85bcd592e8533ecb1d697d6e06054eed27
asif <aasifkhan02@gmail.com>
2024-04-02 06:10:12 +0530

    Initial setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Source Code Analysis Project',
+    version = '0.0.0',
+    autho = "Asif-AI",
+    author_email = 'aasifkhan02@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
